214e21780c0462fd4d43b0a466326361403cbd3b
Christian Maglangit <christianbolohanmaglangit@gmail.com>
2024-10-20 22:37:57 -0700

    minor update

--- src/components/NavHead.js
+++ src/components/NavHead.js
@@ -65,31 +65,13 @@ function Navhead() {
   return (
     <div className="navhead">
       <ul className="aea">
-        <li
-          onClick={() => handleNavigation("home")}
-          className={activeSection === "home" ? "active" : ""}
-        >
-          Home
-        </li>
-        <li
-          onClick={() => handleNavigation("add")}
-          className={activeSection === "add" ? "active" : ""}
-        >
-          Add list
-        </li>
-        <li
-          onClick={() => handleNavigation("edit")}
-          className={activeSection === "edit" ? "active" : ""}
-        >
-          Edit list
-        </li>
+        <li onClick={() => handleNavigation("home")} className={activeSection === "home" ? "active" : ""}>Home</li>
+        <li onClick={() => handleNavigation("add")} className={activeSection === "add" ? "active" : ""}>Add list</li>
+        <li onClick={() => handleNavigation("edit")} className={activeSection === "edit" ? "active" : ""}>Edit list</li>
       </ul>
 
       {/* Home Section */}
-      <div
-        style={{ display: activeSection === "home" ? "block" : "none" }}
-        id="home"
-      >
+      <div style={{ display: activeSection === "home" ? "block" : "none" }} id="home">
         <div className="container_menu_main">
           <div className="menu_main">
             <div className="title">
@@ -100,20 +82,16 @@ function Navhead() {
                 <p>No products added yet.</p>
               ) : (
                 products.map((product, index) => (
-                  <div key={index} class="item">
+                  <div key={index} className="item">
                     <h4>{product.productName}</h4>
                     <p>Brand: {product.brand}</p>
                     <p>Price: ${product.price}</p>
                     <p>Quantity: {product.quantity}</p>
                     <p>Weight/Volume: {product.weightVolume}</p>
                     <p>Store: {product.store}</p>
-                    <div class="edit_remove">
-                      <button onClick={() => handleEditProduct(index)}>
-                        Edit
-                      </button>
-                      <button onClick={() => handleRemoveProduct(index)}>
-                        Remove
-                      </button>
+                    <div className="edit_remove">
+                      <button onClick={() => handleEditProduct(index)}>Edit</button>
+                      <button onClick={() => handleRemoveProduct(index)}>Remove</button>
                     </div>
                   </div>
                 ))
@@ -131,103 +109,26 @@ function Navhead() {
       >
         <h4>Add Product</h4>
         <div className="addItem">
-          <input
-            type="text"
-            name="productName"
-            placeholder="Product Name"
-            value={newProduct.productName}
-            onChange={handleInputChange}
-          />
-          <input
-            type="text"
-            name="brand"
-            placeholder="Brand"
-            value={newProduct.brand}
-            onChange={handleInputChange}
-          />
-          <input
-            type="number"
-            name="price"
-            placeholder="Price"
-            value={newProduct.price}
-            onChange={handleInputChange}
-          />
-          <input
-            type="text"
-            name="weightVolume"
-            placeholder="Weight/Volume"
-            value={newProduct.weightVolume}
-            onChange={handleInputChange}
-          />
-          <input
-            type="text"
-            name="quantity"
-            placeholder="Quantity"
-            value={newProduct.quantity}
-            onChange={handleInputChange}
-          />
-          <input
-            type="text"
-            name="store"
-            placeholder="Store"
-            value={newProduct.store}
-            onChange={handleInputChange}
-          />
-          <button onClick={handleAddProduct}>
-            {isEditing ? "Update" : "Add"}
-          </button>
+          <input type="text" name="productName" placeholder="Product Name" value={newProduct.productName} onChange={handleInputChange} />
+          <input type="text" name="brand" placeholder="Brand" value={newProduct.brand} onChange={handleInputChange} />
+          <input type="number" name="price" placeholder="Price" value={newProduct.price} onChange={handleInputChange} />
+          <input type="text" name="weightVolume" placeholder="Weight/Volume" value={newProduct.weightVolume} onChange={handleInputChange} />
+          <input type="text" name="quantity" placeholder="Quantity" value={newProduct.quantity} onChange={handleInputChange} />
+          <input type="text" name="store" placeholder="Store" value={newProduct.store} onChange={handleInputChange} />
+          <button onClick={handleAddProduct}>{isEditing ? "Update" : "Add"}</button>
         </div>
       </div>
 
       {/* Edit Product Section */}
-      <div
-        style={{ display: activeSection === "edit" ? "flex" : "none" }}
-        id="editproduct"
-      >
-        <h4>Edit Product</h4>
+      <div style={{ display: activeSection === "edit" ? "flex" : "none" }} id="editproduct">
+      <h4>Edit Product</h4>
         <div className="editItem">
-          <input
-            type="text"
-            name="productName"
-            placeholder="Product Name"
-            value={newProduct.productName}
-            onChange={handleInputChange}
-          />
-          <input
-            type="text"
-            name="brand"
-            placeholder="Brand"
-            value={newProduct.brand}
-            onChange={handleInputChange}
-          />
-          <input
-            type="number"
-            name="price"
-            placeholder="Price"
-            value={newProduct.price}
-            onChange={handleInputChange}
-          />
-          <input
-            type="text"
-            name="weightVolume"
-            placeholder="Weight/Volume"
-            value={newProduct.weightVolume}
-            onChange={handleInputChange}
-          />
-          <input
-            type="text"
-            name="quantity"
-            placeholder="Quantity"
-            value={newProduct.quantity}
-            onChange={handleInputChange}
-          />
-          <input
-            type="text"
-            name="store"
-            placeholder="Store"
-            value={newProduct.store}
-            onChange={handleInputChange}
-          />
+          <input type="text" name="productName" placeholder="Product Name" value={newProduct.productName} onChange={handleInputChange} />
+          <input type="text" name="brand" placeholder="Brand" value={newProduct.brand} onChange={handleInputChange} />
+          <input type="number" name="price" placeholder="Price" value={newProduct.price} onChange={handleInputChange} />
+          <input type="text" name="weightVolume" placeholder="Weight/Volume" value={newProduct.weightVolume} onChange={handleInputChange} />
+          <input type="text" name="quantity" placeholder="Quantity" value={newProduct.quantity} onChange={handleInputChange} />
+          <input type="text" name="store" placeholder="Store" value={newProduct.store} onChange={handleInputChange} />
           <button onClick={handleAddProduct}>Update</button>
         </div>
       </div>
